5b0f693bef7dac7c502a47144ee341a337faca45
Derek Kramer <derekdevelops@gmail.com>
2018-06-02 14:51:54 -0600

    :tada: Local authentication working with Feathers back-end

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-toolbar
+      app>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018 Derek Kramer and Dan Beerman</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      fixed: false,
+    }
+  },
+  mounted() {
+    this.authenticate()
+    .then((result) => {
+      console.log('LOGGED IN');
+      this.$router.push('/articles');
+    }).catch(e => {
+      console.error('Authentication error', e);
+    });
+  },
+  methods: {
+    ...mapActions('auth', ['authenticate'])
+  },
+};
+</script>
---
+++ src/router.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Register from './views/Register.vue';
+import Login from './views/Login.vue';
+import Articles from './views/Articles.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/articles',
+      name: 'articles',
+      component: Articles,
+    },
+  ],
+});
